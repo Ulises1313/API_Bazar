39b22d170228a314787f3e7f24f9c64ec1605e55
IDGS-901-17001740 <17001740@alumnos.utleon.edu.mx>
2023-11-14 13:48:53 -0600

    api 4

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173","https://bazar-online-react.netlify.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
